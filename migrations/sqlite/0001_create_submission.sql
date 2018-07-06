0a18dacd8bc6e2bcc4f4e02e79b82d1ff3f1f18e
Robert Czechowski <czechowski@bwinf.de>
2018-07-06 20:29:10 +0200

    Remove column 'contest' from DB table 'submission'

--- migrations/sqlite/0001_create_submission.sql
+++ migrations/sqlite/0001_create_submission.sql
@@ -2,7 +2,6 @@ CREATE TABLE submission (
        id INTEGER PRIMARY KEY,
        session_user INTEGER NOT NULL,
        task INTEGER NOT NULL,
-       contest INTEGER NOT NULL,
        grade INTEGER NOT NULL,
        validated INTEGER NOT NULL,
        nonvalidated_grade INTEGER NOT NULL,
